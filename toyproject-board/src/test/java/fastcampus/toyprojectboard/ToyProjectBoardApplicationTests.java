68c3c3eec87183887432954302d20f2c0d293b9c
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-07-17 16:05:07 +0900

    #3 - 스프링 프로젝트 시작하기
    스프링 프로젝트를 시작한다.
    
    * 데브툴즈
    * 스프링 웹
    * 롬복
    * 액츄에이터

---
+++ toyproject-board/src/test/java/fastcampus/toyprojectboard/ToyProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.toyprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
