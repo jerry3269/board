7b1e517285ea6a7523baa4582cc8715f149370a5
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-07-19 18:53:40 +0900

    #1 - data rest 설정
    * 기본 생성 경로를 `/api`로 설정함
    
    * @RepositoryRestResource를 추가하여 annotation으로 api를 생성할 리포지토리를 설정

--- src/main/java/fastcampus/board/repository/ArticleCommentRepository.java
+++ src/main/java/fastcampus/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package fastcampus.board.repository;
 
 import fastcampus.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
