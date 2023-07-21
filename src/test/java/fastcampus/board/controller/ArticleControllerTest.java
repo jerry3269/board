b3b9f5ad866dacf60fb07d6f937521ec3eaf196f
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-07-21 14:47:06 +0900

    #15 - 뷰 페이지 테스트 업데이트
    contentType 으로 text_html 뒤에 부가적인 요소가 올 가능 성이 있으므로 contentType -> contentTypeCompatibleWith으로 변경

--- src/test/java/fastcampus/board/controller/ArticleControllerTest.java
+++ src/test/java/fastcampus/board/controller/ArticleControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("뷰 컨트롤러 - 게시글 테스트")
-@WebMvcTest(ArticleControllerTest.class)
+@WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
     private final MockMvc mvc;
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 리스트 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestArticlesView_thenRequestArticlesView() throws Exception {
@@ -30,7 +29,7 @@ class ArticleControllerTest {
         //when && then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -45,7 +44,7 @@ class ArticleControllerTest {
         //when && then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -60,7 +59,7 @@ class ArticleControllerTest {
         //when && then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -73,7 +72,7 @@ class ArticleControllerTest {
         //when && then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 
