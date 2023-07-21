3e1385bdf41b6c51edf24c796fa0dd252abf5bf7
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-07-21 14:48:27 +0900

    $15 - 게시글 페이지 기본 골격 작성
    articles 리스트(게시판) 페이지의 기본 골격을 작성함. 
    controller 부분은 아직 미완. 디자인은 향후 css 를 통해 추가할 예정

--- src/main/java/fastcampus/board/controller/ArticleController.java
+++ src/main/java/fastcampus/board/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package fastcampus.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * /articles
@@ -13,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap model) {
+        model.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 
 }
