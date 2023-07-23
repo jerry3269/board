93e86e29b90f904b08b0c001afee38e189f2bc27
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-07-23 15:09:50 +0900

    #18 - 안 쓰는 import 정리

--- src/main/java/fastcampus/board/domain/Article.java
+++ src/main/java/fastcampus/board/domain/Article.java
@@ -1,15 +1,10 @@
 package fastcampus.board.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
