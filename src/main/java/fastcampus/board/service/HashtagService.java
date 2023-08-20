520c3975d5fadcf9707b79b48c55eebe43d5083c
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-08-20 17:37:38 +0900

    #53 - - 해시태그 서비스 구현
    게시글에 달린 해시태그를 관리하기 위한 기능을 구현.
    독특하게도 해시태그의 저장을 다루는 메소드가 없는데
    해시태그의 저장은 현재 비즈니스 로직의 구조 상
    게시글이 저장, 수정될 때만 일어나는 기능으로
    연관관계 매핑(전이)을 통해 게시글 서비스에서 모두 처리가 되기 때문.
    
    * 해시태그 이름으로 해시태그 정보를 db 조회하기
    * 본문에서 해시태그 파싱하기
    * 게시글이 더 이상 없는 해시태그를 삭제하기
    * TODO: 삭제 기능의 테스트를 만들지 않았다. 추가하면 좋을 듯.

--- src/main/java/fastcampus/board/service/HashtagService.java
+++ src/main/java/fastcampus/board/service/HashtagService.java
@@ -1,20 +1,51 @@
 package fastcampus.board.service;
 
+import fastcampus.board.domain.Hashtag;
+import fastcampus.board.repository.HashtagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class HashtagService {
 
-    public Object parseHashtagNames(String content) {
-        return null;
+    private final HashtagRepository hashtagRepository;
+
+    public Set<Hashtag> findHashtagsByNames(Set<String> hashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(hashtagNames));
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
-        return null;
+    @Transactional
+    public Set<String> parseHashtagNames(String content) {
+
+        if (content == null) {
+            return Set.of();
+        }
+
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+");
+        Matcher matcher = pattern.matcher(content.strip());
+        Set<String> result = new HashSet<>();
+
+        while (matcher.find()) {
+            result.add(matcher.group().replace("#", ""));
+        }
+
+        return result;
     }
 
-    public void deleteHashtagWithoutArticles(Object any) {
+    @Transactional
+    public void deleteHashtagWithoutArticles(Long hashtagId) {
+        Hashtag hashtag = hashtagRepository.getReferenceById(hashtagId);
+        if (hashtag.getArticles().isEmpty()) {
+            hashtagRepository.delete(hashtag);
+        }
     }
+
 }
